b416d078471223bc7d9527d98e7c5c876ca3c5c1
bordon1995 <bordon1995@gmail.com>
2024-04-29 21:03:27 -0300

    last2

--- funciones/app.php
+++ funciones/app.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once  __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config/funcion.php';
 require_once __DIR__ . '/../config/db.php';
 
